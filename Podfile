1e7926492cf7424964d3c96be607f2a516cbf704
RollbarExample <wahid.ali@psquickit.com>
2018-01-19 14:10:10 +0530

    comitted by wahid

---
+++ Podfile
@@ -0,0 +1,9 @@
+
+platform :ios, '7.0'
+
+target 'Rollbar-Example' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+ 
+ use_frameworks!
+pod "Rollbar", "~> 1.0.0-alpha"
+end
---
+++ Rollbar-ExampleTests/Rollbar_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rollbar_ExampleTests.swift
+//  Rollbar-ExampleTests
+//
+//  Created by PS QuickIT on 1/19/18.
+//  Copyright © 2018 PS QuickIT. All rights reserved.
+//
+
+import XCTest
+@testable import Rollbar_Example
+
+class Rollbar_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
